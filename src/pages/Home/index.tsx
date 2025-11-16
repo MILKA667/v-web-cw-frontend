5f842b5f43235f88476e837653fdf16654ee2387
MILKA667 <coromyslov.andrey2005@gmail.com>
2025-11-16 18:27:34 +0300

    feat: add drag-and-drop file drop zone

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,13 +2,16 @@ import './style.css'
 import FileDropzone from '../../components/FileDropzone'
 import SearchParams from '../../components/SearchParams'
 import Result from '../../components/Result'
+import { FileProvider } from '../../contexts/FileContext'
 function Home(){
     return(
+        <FileProvider>
         <div className="main_container">
             <div className="drop_zone_container"><FileDropzone /></div>
             <div className="settings_container"><SearchParams /></div>
             <div className="result_container"><Result /></div>
         </div>
+        </FileProvider>
     )
 }
 export default Home
