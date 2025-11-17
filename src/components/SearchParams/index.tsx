5c2dcb2b13f65a95650231cb2cff43a806685b7b
MILKA667 <coromyslov.andrey2005@gmail.com>
2025-11-17 12:57:44 +0300

    feat: добавлен функционал загрузки файла

--- src/components/SearchParams/index.tsx
+++ src/components/SearchParams/index.tsx
@@ -1,22 +1,59 @@
+import { useContext } from "react";
+import { FileContext } from "../../contexts/FileContext";
 import './style.css'
+const URL = 'http:localhost:5000/api/'
 
-function SearchParams(){
-    return(
+export default function SearchParams() {
+    const { file } = useContext(FileContext)!
+
+    async function upload_file() {
+        if (!file) {
+            alert('Вы не загрузили файл!')
+        } else {
+            const formData = new FormData();
+            formData.append("file", file);
+
+            const response = await fetch(URL + 'upload_file', {
+                method: "POST",
+                body: formData,
+            });
+
+            const data = await response.json();
+        }
+
+    }
+
+
+    return (
         <div className='settings_container'>
             <p>Настройки поиска</p>
-            <button>Сбросить все</button>
-            <div className='radio_button_container'>
-                <input type="radio" id="film" name="search_settings" />
-                <label htmlFor="film">Фильм</label>
-                <input type="radio" id="anime" name="search_settings" />
-                <label htmlFor="anime">Аниме</label>
-                <input type="radio" id="tvshow" name="search_settings" />
-                <label htmlFor="tvshow">Сериал</label>
-                <input type="radio" id="music" name="search_settings" />
-                <label htmlFor="music">Музыка</label>
+            <button onClick={upload_file}>Поиск</button>
+
+            <div className='radio-group'>
+                <label className='radio'>
+                    <input type="radio" name="search_settings" />
+                    <span className='radio-custom'></span>
+                    Фильм
+                </label>
+
+                <label className='radio'>
+                    <input type="radio" name="search_settings" />
+                    <span className='radio-custom'></span>
+                    Аниме
+                </label>
+
+                <label className='radio'>
+                    <input type="radio" name="search_settings" />
+                    <span className='radio-custom'></span>
+                    Сериал
+                </label>
+
+                <label className='radio'>
+                    <input type="radio" name="search_settings" />
+                    <span className='radio-custom'></span>
+                    Музыка
+                </label>
             </div>
         </div>
-    )
+    );
 }
-
-export default SearchParams
